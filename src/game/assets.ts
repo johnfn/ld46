f24966726ea4ca30c457eb264d0318224eec3ddb
johnfn <johnfn@gmail.com>
2020-04-18 20:52:48 -0700

    MERGEEEEEEEEEEEEe

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -22,15 +22,20 @@ export type AssetPath =
   | "music/Withers.mp3"
   | "music.json"
   | "owo.png"
+  | "parallax bg/00 - sky ver2.png"
+  | "parallax bg/00 - sky.png"
+  | "parallax bg/01 - skyline back.png"
+  | "parallax bg/02 - skyline.png"
+  | "parallax bg/03 - mist.png"
+  | "parallax bg/04 - city back.png"
+  | "parallax bg/05 - forest back.png"
+  | "parallax bg/06 - city.png"
+  | "parallax bg/07 - forest.png"
   | "parallax bg/bigcloud01.png"
   | "parallax bg/bigcloud02.png"
   | "parallax bg/bigcloud03.png"
   | "parallax bg/bigcloud04.png"
   | "parallax bg/bigcloud05.png"
-  | "parallax bg/city1.png"
-  | "parallax bg/mist1.png"
-  | "parallax bg/sky1.png"
-  | "parallax bg/skyline1.png"
   | "sound effects/Distant Gears.mp3"
   | "sound effects/step grass 1.mp3"
   | "sound effects/step grass 2.mp3"
@@ -118,39 +123,44 @@ export type AssetPath =
 
 
 export const AssetsToLoad = {
-  "dialog_box"                 : { type: "Image"       as const, path: "dialog_box.png"                  },
-  "interactions"               : { type: "TileMap"     as const, path: "interactions.json"               },
-  "loop1"                      : { type: "Audio"       as const, path: "loop1.mp3"                       },
-  "loop2"                      : { type: "Audio"       as const, path: "loop2.mp3"                       },
-  "map"                        : { type: "TileMap"     as const, path: "map.json"                        },
-  "music/Sanctuary"            : { type: "Audio"       as const, path: "music/Sanctuary.mp3"             },
-  "music/Withers"              : { type: "Audio"       as const, path: "music/Withers.mp3"               },
-  "music"                      : { type: "TileMap"     as const, path: "music.json"                      },
-  "owo"                        : { type: "Image"       as const, path: "owo.png"                         },
-  "parallax bg/bigcloud01"     : { type: "Image"       as const, path: "parallax bg/bigcloud01.png"      },
-  "parallax bg/bigcloud02"     : { type: "Image"       as const, path: "parallax bg/bigcloud02.png"      },
-  "parallax bg/bigcloud03"     : { type: "Image"       as const, path: "parallax bg/bigcloud03.png"      },
-  "parallax bg/bigcloud04"     : { type: "Image"       as const, path: "parallax bg/bigcloud04.png"      },
-  "parallax bg/bigcloud05"     : { type: "Image"       as const, path: "parallax bg/bigcloud05.png"      },
-  "parallax bg/city1"          : { type: "Image"       as const, path: "parallax bg/city1.png"           },
-  "parallax bg/mist1"          : { type: "Image"       as const, path: "parallax bg/mist1.png"           },
-  "parallax bg/sky1"           : { type: "Image"       as const, path: "parallax bg/sky1.png"            },
-  "parallax bg/skyline1"       : { type: "Image"       as const, path: "parallax bg/skyline1.png"        },
-  "sound effects/Distant Gears": { type: "Audio"       as const, path: "sound effects/Distant Gears.mp3" },
-  "sound effects/step grass 1" : { type: "Audio"       as const, path: "sound effects/step grass 1.mp3"  },
-  "sound effects/step grass 2" : { type: "Audio"       as const, path: "sound effects/step grass 2.mp3"  },
-  "sound effects/step grass 3" : { type: "Audio"       as const, path: "sound effects/step grass 3.mp3"  },
-  "sound effects/step stone 1" : { type: "Audio"       as const, path: "sound effects/step stone 1.mp3"  },
-  "sound effects/step stone 2" : { type: "Audio"       as const, path: "sound effects/step stone 2.mp3"  },
-  "sound effects/step stone 3" : { type: "Audio"       as const, path: "sound effects/step stone 3.mp3"  },
-  "sound effects/step wood 1"  : { type: "Audio"       as const, path: "sound effects/step wood 1.mp3"   },
-  "sound effects/step wood 2"  : { type: "Audio"       as const, path: "sound effects/step wood 2.mp3"   },
-  "sound effects/step wood 3"  : { type: "Audio"       as const, path: "sound effects/step wood 3.mp3"   },
-  "spirit_empty_hud"           : { type: "Image"       as const, path: "spirit_empty_hud.png"            },
-  "spirit_full_hud"            : { type: "Image"       as const, path: "spirit_full_hud.png"             },
-  "tileset"                    : { type: "Image"       as const, path: "tileset.png"                     },
-  "vine_flower_source"         : { type: "Image"       as const, path: "vine_flower_source.png"          },
-  "world"                      : { type: "TileWorld"   as const, path: "world.json"                      },
+  "dialog_box"                   : { type: "Image"       as const, path: "dialog_box.png"                    },
+  "interactions"                 : { type: "TileMap"     as const, path: "interactions.json"                 },
+  "loop1"                        : { type: "Audio"       as const, path: "loop1.mp3"                         },
+  "loop2"                        : { type: "Audio"       as const, path: "loop2.mp3"                         },
+  "map"                          : { type: "TileMap"     as const, path: "map.json"                          },
+  "music/Sanctuary"              : { type: "Audio"       as const, path: "music/Sanctuary.mp3"               },
+  "music/Withers"                : { type: "Audio"       as const, path: "music/Withers.mp3"                 },
+  "music"                        : { type: "TileMap"     as const, path: "music.json"                        },
+  "owo"                          : { type: "Image"       as const, path: "owo.png"                           },
+  "parallax bg/00 - sky ver2"    : { type: "Image"       as const, path: "parallax bg/00 - sky ver2.png"     },
+  "parallax bg/00 - sky"         : { type: "Image"       as const, path: "parallax bg/00 - sky.png"          },
+  "parallax bg/01 - skyline back": { type: "Image"       as const, path: "parallax bg/01 - skyline back.png" },
+  "parallax bg/02 - skyline"     : { type: "Image"       as const, path: "parallax bg/02 - skyline.png"      },
+  "parallax bg/03 - mist"        : { type: "Image"       as const, path: "parallax bg/03 - mist.png"         },
+  "parallax bg/04 - city back"   : { type: "Image"       as const, path: "parallax bg/04 - city back.png"    },
+  "parallax bg/05 - forest back" : { type: "Image"       as const, path: "parallax bg/05 - forest back.png"  },
+  "parallax bg/06 - city"        : { type: "Image"       as const, path: "parallax bg/06 - city.png"         },
+  "parallax bg/07 - forest"      : { type: "Image"       as const, path: "parallax bg/07 - forest.png"       },
+  "parallax bg/bigcloud01"       : { type: "Image"       as const, path: "parallax bg/bigcloud01.png"        },
+  "parallax bg/bigcloud02"       : { type: "Image"       as const, path: "parallax bg/bigcloud02.png"        },
+  "parallax bg/bigcloud03"       : { type: "Image"       as const, path: "parallax bg/bigcloud03.png"        },
+  "parallax bg/bigcloud04"       : { type: "Image"       as const, path: "parallax bg/bigcloud04.png"        },
+  "parallax bg/bigcloud05"       : { type: "Image"       as const, path: "parallax bg/bigcloud05.png"        },
+  "sound effects/Distant Gears"  : { type: "Audio"       as const, path: "sound effects/Distant Gears.mp3"   },
+  "sound effects/step grass 1"   : { type: "Audio"       as const, path: "sound effects/step grass 1.mp3"    },
+  "sound effects/step grass 2"   : { type: "Audio"       as const, path: "sound effects/step grass 2.mp3"    },
+  "sound effects/step grass 3"   : { type: "Audio"       as const, path: "sound effects/step grass 3.mp3"    },
+  "sound effects/step stone 1"   : { type: "Audio"       as const, path: "sound effects/step stone 1.mp3"    },
+  "sound effects/step stone 2"   : { type: "Audio"       as const, path: "sound effects/step stone 2.mp3"    },
+  "sound effects/step stone 3"   : { type: "Audio"       as const, path: "sound effects/step stone 3.mp3"    },
+  "sound effects/step wood 1"    : { type: "Audio"       as const, path: "sound effects/step wood 1.mp3"     },
+  "sound effects/step wood 2"    : { type: "Audio"       as const, path: "sound effects/step wood 2.mp3"     },
+  "sound effects/step wood 3"    : { type: "Audio"       as const, path: "sound effects/step wood 3.mp3"     },
+  "spirit_empty_hud"             : { type: "Image"       as const, path: "spirit_empty_hud.png"              },
+  "spirit_full_hud"              : { type: "Image"       as const, path: "spirit_full_hud.png"               },
+  "tileset"                      : { type: "Image"       as const, path: "tileset.png"                       },
+  "vine_flower_source"           : { type: "Image"       as const, path: "vine_flower_source.png"            },
+  "world"                        : { type: "TileWorld"   as const, path: "world.json"                        },
 
   /* Animations */
 
