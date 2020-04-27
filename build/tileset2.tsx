1ba8c4a2305816d44aa7d0528919f3cc4d4345b6
johnfn <johnfn@gmail.com>
2020-04-26 17:38:13 -0700

    Remove build

--- build/tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="tileset2" tilewidth="256" tileheight="256" tilecount="48" columns="8">
- <image source="tileset2.png" width="2048" height="1536"/>
-</tileset>
