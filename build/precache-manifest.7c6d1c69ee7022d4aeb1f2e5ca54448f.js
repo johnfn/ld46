e34a242202c3490e42bce9fdf1e4cb53bd7378fe
johnfn <johnfn@gmail.com>
2020-04-26 17:47:25 -0700

    add final build

---
+++ build/precache-manifest.7c6d1c69ee7022d4aeb1f2e5ca54448f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21cc2919096bb21d2dd25b291a4aed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "679b6dcae3905176c5a4",
+    "url": "/static/js/2.49619a0d.chunk.js"
+  },
+  {
+    "revision": "555486c5c14e9d19a66b1506a7ff28b9",
+    "url": "/static/js/2.49619a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05f4764702c3b472b0c",
+    "url": "/static/js/main.422b1d0f.chunk.js"
+  },
+  {
+    "revision": "de34723fb7a5a04a6a30",
+    "url": "/static/js/runtime-main.13fc1f21.js"
+  }
+]);
